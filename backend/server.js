c46bff15ebf7c882e942165113d539895a48c10d
jaeson <130809131+Programmer-jason@users.noreply.github.com>
2024-03-25 23:57:34 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const cookie_parser = require('cookie-parser')
 require('dotenv').config()
 
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://anime-databases.onrender.com/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(cookie_parser())
