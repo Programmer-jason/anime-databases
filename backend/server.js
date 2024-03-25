1f07ea90b9f5035aecb6f46349cd3afd56bfb770
jaeson <130809131+Programmer-jason@users.noreply.github.com>
2024-03-25 23:53:39 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const cookie_parser = require('cookie-parser')
 require('dotenv').config()
 
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
 app.use(cookie_parser())
 
 //ADMIN ROUTE
